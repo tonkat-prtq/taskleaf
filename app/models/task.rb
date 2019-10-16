98c002eb6abf6e46bc896227c496f1842f8f3f1e
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-10-17 02:06:13 +0900

    1対多、ログインしてるユーザーだけのtaskデータ読み出し

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_conma
 
+  belongs_to :user
+
   private
 
   def validate_name_not_including_conma
