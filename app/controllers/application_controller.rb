0704d93229515b972a4ad655527be102033f93e9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-10-17 02:55:26 +0900

    いろいろ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def login_required
-    redirect_to login_path, unless current_user
+    redirect_to login_path unless current_user
   end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, destroy]
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,12 +12,10 @@ class TasksController < ApplicationController
   end
 
   def edit # 編集画面を表示するeditアクション
-    @task = current_user.tasks.find(params[:id]) # URL内に含まれるタスクのidをパラメータから受け取ってデータベースを検索
   end
 
   def update # 画面から送られてきたデータを使ってデータベースを更新するupdateアクション
-    task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
+    @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク 「#{task.name}」を更新しました。"
   end
 
@@ -32,8 +30,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
+    @task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
@@ -42,4 +39,8 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:name, :description)
   end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  endeggu
 end
