83a6f751b4589d7f5c246d4cf583690b36ffc5c7
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-10-28 08:58:59 +0900

    ユーザーAのタスクを作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,7 +4,7 @@ describe 'タスク管理機能', type: :system do
   describe '一覧表示機能', do
     before do
       user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') # ユーザーAを作成しておく
-      # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a) # 作成者がユーザーAであるタスクを作成しておく
     end
 
     context 'ユーザーAがログインしているとき' do
